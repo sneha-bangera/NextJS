4c8c2fb0947edec1fb29bbb67b9ea6b2c8d4acf6
Sneha Bangera <snehabangera05@gmail.com>
2025-08-01 19:09:49 +0530

    login changes

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,11 +1,11 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState , Suspense} from "react";
 import styles from "./page.module.css";
 import { getProviders, signIn, useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
 
-const Login = ({ url }) => {
+const LoginContent = ({ url }) => {
   const session = useSession();
   const router = useRouter();
   const params = useSearchParams();
@@ -83,6 +83,14 @@ const Login = ({ url }) => {
         Login with Github
       </button> */}
     </div>
+  );
+};
+
+const Login = () => {
+  return (
+    <Suspense fallback={<div>Loading login...</div>}>
+      <LoginContent />
+    </Suspense>
   );
 };
 
